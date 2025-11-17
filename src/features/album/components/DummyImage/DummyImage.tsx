12a3eca65e8753116d9fc0dcaf21d65e2099d51a
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-17 20:34:59 +0900

    refactor(album): change rounded style

--- src/features/album/components/DummyImage/DummyImage.tsx
+++ src/features/album/components/DummyImage/DummyImage.tsx
@@ -1,8 +1,8 @@
 export function DummyImage(): React.JSX.Element {
   return (
     <div>
-      <div className="min-h-80 px-4 rounded-lg bg-monotone-200" />
-      <div className="flex justify-center h-4 rounded-lg mt-2 mr-6 bg-monotone-200" />
+      <div className="min-h-80 px-4 rounded-2 bg-monotone-200" />
+      <div className="flex justify-center h-4 rounded-2 mt-2 mr-6 bg-monotone-200" />
     </div>
   );
 }
