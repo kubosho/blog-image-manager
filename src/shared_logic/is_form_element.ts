10fbd79bb2ed9b84e3d2cca9fdf11a0fdaba0a98
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-21 18:53:49 +0900

    chore: remove unused files

--- src/shared_logic/is_form_element.ts
+++
@@ -1,6 +0,0 @@
-const FORM_ELEMENT_NAMES = Object.freeze(['BUTTON', 'INPUT', 'TEXTAREA', 'SELECT']);
-
-export function isFormElement({ tagName }: Pick<Element, 'tagName'>): boolean {
-  const targetNodeName = tagName.toUpperCase();
-  return FORM_ELEMENT_NAMES.includes(targetNodeName);
-}
