eb26c051bbf17c0a5e24c9ff3ec3c4b24a2a5429
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-21 16:49:16 +0900

    feat(album): re-implement ImageUploadButton

---
+++ src/features/album/components/ImageUploadButton/ImageUploadButton.stories.tsx
@@ -0,0 +1,10 @@
+import preview from '../../../../../.storybook/preview';
+import { ImageUploadButton } from '.';
+
+const meta = preview.meta({
+  component: ImageUploadButton,
+});
+
+export const Basic = meta.story({
+  args: {},
+});
---
+++ src/features/album/components/ImageUploadButton/ImageUploadButton.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { FileUpload } from '@ark-ui/react/file-upload';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+
+import { useImageUploader } from '../../hooks/use-image-uploader';
+
+export const ImageUploadButton = (): React.JSX.Element => {
+  const [filesToUpload, setFilesToUpload] = useState<File[]>([]);
+  const { uploadImage } = useImageUploader();
+
+  useEffect(() => {
+    if (filesToUpload.length === 0) {
+      return;
+    }
+
+    void uploadImage(filesToUpload).then(() => {
+      setFilesToUpload([]);
+    });
+  }, [filesToUpload]);
+
+  const handleFileAccept = (details: FileUpload.FileAcceptDetails): void => {
+    setFilesToUpload(details.files);
+  };
+
+  return (
+    <FileUpload.Root accept="image/*" allowDrop={false} maxFiles={5} onFileAccept={handleFileAccept}>
+      <FileUpload.HiddenInput />
+      <FileUpload.Trigger className="inline-flex items-center justify-center gap-1 px-4 py-2 rounded-2 shadow-md bg-blue-600 text-monotone-100">
+        <Image src="/images/icons/plus.svg" alt="" width={12} height={12} />
+        Add file(s)
+      </FileUpload.Trigger>
+    </FileUpload.Root>
+  );
+};
