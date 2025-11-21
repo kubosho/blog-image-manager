e03c85dc0775ce3a6f18a99b1d2e147a3a3fef21
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-21 16:49:39 +0900

    fix(auth): fix lint

--- src/features/auth/server-actions.ts
+++ src/features/auth/server-actions.ts
@@ -4,10 +4,10 @@ import type { ProviderId } from 'next-auth/providers';
 
 import { signIn, signOut } from './auth';
 
-export async function handleSignIn(providerId: ProviderId) {
+export async function handleSignIn(providerId: ProviderId): Promise<void> {
   await signIn(providerId);
 }
 
-export async function handleSignOut() {
+export async function handleSignOut(): Promise<void> {
   await signOut();
 }
