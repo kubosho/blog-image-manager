4694568e9d890d62203abe1f03535f4748b14c1c
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-21 18:59:20 +0900

    feat(components): rewrite SiteHeader storybook

---
+++ src/components/SiteHeader/SiteHeader.stories.tsx
@@ -0,0 +1,10 @@
+import preview from '../../../.storybook/preview';
+import { SiteHeader } from '.';
+
+const meta = preview.meta({
+  component: SiteHeader,
+});
+
+export const Basic = meta.story({
+  args: {},
+});
