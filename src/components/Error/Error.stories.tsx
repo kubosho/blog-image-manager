67413b4d305280cdddf0e86016735b7820cc90aa
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-22 14:20:20 +0900

    refactor(component): rewrite story for Error component

---
+++ src/components/Error/Error.stories.tsx
@@ -0,0 +1,19 @@
+import preview from '../../../.storybook/preview';
+import { ERROR_REASON } from '../../constants/error_reason';
+import { Error } from '../Error';
+
+const meta = preview.meta({
+  component: Error,
+});
+
+export const ErrorWithNotSetAwsEnvironmentVariables = meta.story({
+  args: {
+    errorReason: ERROR_REASON.NOT_SET_AWS_ENVIRONMENT_VARIABLES,
+  },
+});
+
+export const ErrorWithGeneralError = meta.story({
+  args: {
+    errorReason: ERROR_REASON.GENERAL_ERROR,
+  },
+});
