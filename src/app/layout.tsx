781f3051be44e4364c33fde327f3cb799568bc22
KUBOTA Shota <ta2+github@o2p.jp>
2025-11-25 01:18:50 +0900

    feat(app): add format detection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,18 @@
 import './globals.css';
 
+import type { Metadata } from 'next';
+
 type Props = {
   children: React.ReactNode;
+};
+
+export const metadata: Metadata = {
+  formatDetection: {
+    telephone: false,
+    date: false,
+    email: false,
+    address: false,
+  },
 };
 
 export default function RootLayout({ children }: Props): React.JSX.Element {
